2b369a73cd3745fb1d16e7c6f49ad4ac6b499b97
94dreamer <503633021@qq.com>
2017-03-15 11:53:34 +0800

    import webpack 2.

---
+++ stories/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {storiesOf, action, linkTo} from '@kadira/storybook';
+import Button from './Button';
+import Welcome from './Welcome';
+// import {Button} from '../src/DreamUIReact';
+
+storiesOf('Welcome', module)
+  .add('to Storybook', () => (
+    <Welcome showApp={linkTo('Button')}/>
+  ));
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+  ));
+
+
+
+
