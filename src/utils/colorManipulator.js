de95c1f9821d5ab2fe0cb90b568ad2bd37b337e0
94dreamer <503633021@qq.com>
2017-03-18 21:33:16 +0800

    inline style css

--- src/utils/colorManipulator.js
+++ src/utils/colorManipulator.js
@@ -2,3 +2,21 @@
  * Created by zhouzhen on 2017/3/17.
  */
 import warning from 'warning';
+
+
+/**
+ * 返回 一个值和数值范围后的允许值
+ * @param {number} value
+ * @param {number}min
+ * @param {number}max
+ * @returns {number}
+ */
+function clamp(value, min, max) {
+  if (value < min) {
+    return min;
+  }
+  if (value > max) {
+    return max;
+  }
+  return value;
+}
