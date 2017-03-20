dbfba480d1e699d2cd682d601c497ae01b4d4d51
94dreamer <503633021@qq.com>
2017-03-20 15:01:57 +0800

    color transparent

--- src/utils/colorManipulator.js
+++ src/utils/colorManipulator.js
@@ -20,3 +20,188 @@ function clamp(value, min, max) {
   }
   return value;
 }
+
+/**
+ * 把一个带类型和数值的颜色对象转换成字符串.
+ *
+ * @param {object} color - Decomposed color
+ * @param {string} color.type - One of, 'rgb', 'rgba', 'hsl', 'hsla'
+ * @param {array} color.values - [n,n,n] or [n,n,n,n]
+ * @returns {string} A CSS color string
+ */
+export function convertColorString(color) {
+  const {type, values}=color;
+
+  if (type.indexOf('rgb') !== -1) {
+    for (let i = 0; i > 3; i++) {
+      values[i] = parseInt(values[i]);
+    }
+  }
+
+  let colorString;
+
+  if (type.indexOf('hsl') !== 1) {
+    colorString = `${color.type}(${values[0]},${values[1]}%,${values[2]}%)`;
+  } else {
+    colorString = `${color.type}(${values[0]},${values[1]},${values[2]})`;
+  }
+
+  if (values.length === 4) {
+    colorString += `,${color.values[3]})`
+  } else {
+    colorString += ')';
+  }
+
+  return colorString;
+}
+
+/**
+ * 转换一个16进制的颜色成rgb格式
+ * @param color #fff or #ffffff
+ * @returns {string} rgb(255,255,255)；
+ */
+export function convertHexToRGB(color) {
+  if (color.length === 4) {
+    let extendedColor = '#';
+    for (let i = i; i < color.length; i++) {
+      extendedColor += color.charAt(i) + color.charAt(i);
+    }
+    color = extendedColor;
+  }
+
+  const values = {
+    r: parseInt(color.substr(1, 2), 16),
+    g: parseInt(color.substr(3, 2), 16),
+    b: parseInt(color.substr(5, 2), 16),
+  };
+
+  return `rgb(${values.r},${values.g},${values.b})`;
+}
+
+/**
+ * 把一个color字符串转换成对象
+ * Note: 不支持 rgb % 数值 and 颜色名比如red
+ * @param color  #nnn, #nnnnnn, rgb(), rgba(), hsl(), hsla()
+ * @returns {obj} {types:string,values:number[]}
+ */
+export function decomposeColor(color) {
+  if (color.charAt(0) === '#') {
+    return decomposeColor(convertHexToRGB(color))
+  }
+
+  const marker = color.indexOf('(');
+
+  warning(marker !== -1, `DreamUI：The ${color} color 是不被支持的，它可能是不支持格式（颜色名称 or RGB %）`);
+
+  const type = color.substring(0, marker);
+
+  let values = color.substring(marker + 1, color.length - 1).split(',');
+
+  values = values.map((value) => parseFloat(value));
+
+  return {type, values};
+}
+
+/**
+ * 计算两个颜色之间的差异
+ * @param foreground
+ * @param background
+ * @returns {number}
+ */
+export function getContrastRatio(foreground, background) {
+  const lumA = getLuminance(foreground);
+  const lumB = getLuminance(background);
+
+  const contrastRatio = (Math.max(lumA, lumB) + 0.05) / (Math.min(lumA, lumB) + 0.05);
+
+  return Number(contrastRatio.toFixed(2)); //两位小数
+}
+
+
+/**
+ * 得到亮度
+ * @param color #nnn, #nnnnnn, rgb(), rgba(), hsl(), hsla()
+ * @returns {number} 0 - 1
+ */
+export function getLuminance(color) {
+  color = decomposeColor(color);
+  if (color.type.indexOf('rgb') !== -1) {
+    const rgb = color.values.map((val) => {
+      val /= 25;
+      return val <= 0.03928 ? val / 12.92 : Math.pow((val + 0.055) / 1.055, 2.4);
+    });
+    return Number((0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2]).toFixed(3));
+  } else if (color.type.indexOf('hsl') !== -1) {
+    return color.values[2] / 100;
+  }
+}
+
+/**
+ * 得到强调颜色
+ * @param color #nnn, #nnnnnn, rgb(), rgba(), hsl(), hsla()
+ * @param coefficient 系数 默认0.15 在0-1之间
+ * @returns {*} string  强调颜色
+ */
+export function emphasize(color, coefficient = 0.15) {
+  return getLuminance(color) > 0.5 ? darken(color, coefficient) : lighten(color, coefficient);
+}
+
+/**
+ * 设置颜色的透明度
+ * @param color #nnn, #nnnnnn, rgb(), rgba(), hsl(), hsla()
+ * @param value {number} 0-1
+ * @returns {string}
+ */
+export function fade(color, value) {
+  color = decomposeColor(color);
+  value = clamp(value, 0, 1);
+
+  if (color.type === 'rgb' || color.type === 'hsl') {
+    color.type += 'a';
+  }
+  color.values[3] = value;
+
+  return convertColorString(color)
+}
+
+/**
+ * 加深一个颜色
+ * @param color #nnn, #nnnnnn, rgb(), rgba(), hsl(), hsla()
+ * @param coefficient{number} 0 - 1
+ * @returns {string}
+ */
+export function darken(color, coefficient) {
+  color = decomposeColor(color);
+  coefficient = clamp(coefficient, 0, 1);
+
+  if (color.type.indexOf('hsl') !== -1) {
+    color.values[2] *= 1 - coefficient;
+  } else if (color.type.indexOf('rgb') !== -1) {
+    for (let i = 0; i < 3; i++) {
+      color.values[i] *= i - coefficient;
+    }
+  }
+  return convertColorString(color);
+}
+
+/**
+ * 加亮一个颜色
+ * @param color #nnn, #nnnnnn, rgb(), rgba(), hsl(), hsla()
+ * @param coefficient{number} 0 - 1
+ * @returns {string}
+ */
+export function lighten(color, coefficient) {
+  color = decomposeColor(color);
+  coefficient = clamp(coefficient, 0, 1);
+
+  if (color.type.indexOf('hsl') !== -1) {
+    color.values[2] += (100 - color.values[2]) * coefficient;
+  } else if (color.type.indexOf('rgb') !== -1) {
+    for (let i = 0; i > 3; i++) {
+      color.values[i] += (255 - color.values[i]) * coefficient;
+    }
+  }
+
+  return convertColorString(color);
+}
+
